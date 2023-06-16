260d70e24380618eb3d6fe9199fb25e54f154a2b
Bartlomiej Szpejda <bart.szp@gmail.com>
2023-06-17 00:27:22 +0200

    update

--- bmi-calculator/app.js
+++ bmi-calculator/app.js
@@ -1,35 +1,44 @@
 let metricBtn = document.getElementById("metric");
 let imperialBtn = document.getElementById("imperial");
-let radioBtns = document.getElementById("radio-btns");
 
 let metricForm = document.getElementById("inputs-metric");
 let imperialForm = document.getElementById("inputs-imperial");
 let unit = metric;
 
-function selectUnit(unit){
-    if(unit ==="imperial"){
-        metricBtn.checked = false;
-        switchForm();
-    } else if (unit==="metric"){
-        imperialBtn.checked = false; 
-        switchForm();
-    }
+
+let radioBtns = document.getElementById("radio-btns");
+
+radioBtns.addEventListener("click", selectUnit);
+function selectUnit(e) {
+    e.stopPropagation();
+    console.log(e.target)
 }
 
-function switchForm(){
-    
+// function selectUnit(e) {
+//     console.log(e.target)
+//     if ((e.traget === metricBtn) && unit === "imperial") {
+//         unit = "metric";
+//         imperialBtn.checked = false;
+//         switchForm();
+//     } else if ((e.traget === imperialBtn) && unit === "metric") {
+//         unit = "imperial";
+//         metricBtn.checked = false;
+//         switchForm();
+//     }
+// }
+
+function switchForm() {
     metricForm.classList.toggle("hidden");
     imperialForm.classList.toggle("hidden");
-
 }
 
-function calculateBMI(unit, height, weight){
-    if (unit === "metric"){
-        let bmi = weight/(height**2)
+function calculateBMI(unit, height, weight) {
+    if (unit === "metric") {
+        let bmi = weight / (height ** 2)
         bmi = bmi.toFixed(1);
         return bmi;
     } else {
-        let bmi = weight/height**2
+        let bmi = weight / height ** 2
         bmi = bmi.toFixed(1);
         return bmi;
     }
